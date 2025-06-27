0c4cb5c8665fd389fbd4281e4d61843e04e3656d
spencer burke <info.spencer.burke@gmail.com>
2025-06-26 23:51:02 -0700

    Add the svelte files with better formatting

---
+++ src/web/src/lib/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+	let {loggedIn = false} = $props();
+</script>
+
+<nav>
+	<div class="brand">
+		<p>Minflix</p>
+	</div>
+	<div class="navigation-buttons">
+		{#if loggedIn}
+			<button>Logout</button>
+		{:else}
+			<button>Login</button>
+			<button>Register</button>
+		{/if}
+	</div>
+</nav> 
